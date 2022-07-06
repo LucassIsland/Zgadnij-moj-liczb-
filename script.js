7b176578ece3696db6cc2b918aae1c0044d167ac
Lukasz Watroba <108336492+LucassIsland@users.noreply.github.com>
2022-07-06 07:28:48 +0200

    Add files via upload
    It's final version.

--- script.js
+++ script.js
@@ -1,73 +1,56 @@
 'use strict';
 
-// console.log(
-//   (document.querySelector('.message').textContent = '❤ Correct Answer!')
-// );
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-
-// console.log(document.querySelector('.guess').value);
-
 let secretNumber = Math.trunc(Math.random() * 100) + 1;
-
 let score = 20;
-
 let highScore = 0;
 
-// document.querySelector('.number').textContent = secretNumber;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
 
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').textContent =
-    'Jaką liczbę mam na myśli... Wpisz swoją propozycję w prostokąt po lewej stronie.';
+  displayMessage(
+    'Jaką liczbę mam na myśli... Zgadnij i wpisz w prostokąt po lewej stronie😉'
+  );
   document.querySelector('.guess').value = '';
   score = 20;
   document.querySelector('.score').textContent = score;
   secretNumber = Math.trunc(Math.random() * 100) + 1;
-  document.querySelector('.number').textContent = '?';
+  displayNumber('?');
   document.querySelector('.number').style.width = '15rem';
 });
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector('.message').textContent =
-      '📛 Nie wpisałaś żadnej liczny :)';
+    displayMessage('📛 Nie wpisałaś żadnej liczny :)');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '❤ Poprawna odpowiedź!';
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('❤ Poprawna odpowiedź!');
+    displayNumber(secretNumber);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Niestety... za wysoka, próbuje dalej ;)';
+      displayMessage(
+        guess > secretNumber ? 'Nie, za wysoka ;)' : 'Nie, za niska ;)'
+      );
       score--;
       document.querySelector('.score').textContent = score;
+      document.querySelector('.guess').value = '';
     } else {
-      document.querySelector('.message').textContent =
-        '🔥Oh... niestety, tym razem przegrałaś ;/';
+      displayMessage('🔥Oh... niestety, tym razem przegrałaś ;/');
       score = 0;
       document.querySelector('.score').textContent = score;
-      document.querySelector('.number').textContent = secretNumber;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Za niska, próbuj dalej ;)';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent =
-        '🔥Oh... niestety, tym razem przegrałaś ;/';
-      score = 0;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.number').textContent = secretNumber;
+      displayNumber(secretNumber);
     }
   }
 });
